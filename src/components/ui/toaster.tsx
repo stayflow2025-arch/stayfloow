1eb2ccf8c2cfd3dcc3b3b3220f64ce5dd909c5dc
stayflow2025-arch <stayflow2025@gmail.com>
2026-01-31 20:32:47 +0100

    Update toaster.tsx

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -18,14 +18,4 @@ export function Toaster() {
       {toasts.map(({ id, title, description, action, ...props }) => (
         <Toast key={id} {...props}>
           <div className="grid gap-1">
-            {title && <ToastTitle>{title}</ToastTitle>}
-            {description && <ToastDescription>{description}</ToastDescription>}
-          </div>
-          {action}
-          <ToastClose />
-        </Toast>
-      ))}
-      <ToastViewport />
-    </ToastProvider>
-  );
-}
+            {title && <ToastTitle>{title}</
