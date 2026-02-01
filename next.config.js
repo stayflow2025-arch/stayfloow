cf731b28fff1e82f6e5d120b4697eae535958c6d
stayflow2025-arch <stayflow2025@gmail.com>
2026-02-01 12:39:17 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -22,9 +24,6 @@ const nextConfig = {
       },
     ],
   },
-  experimental: {
-    webpackBuildWorker: true
-  }
-}
+};
 
 module.exports = nextConfig;
