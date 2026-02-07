fbcae35ece38f788130523d84cf065b89f618a58
stayflow2025-arch <stayflow2025@gmail.com>
2026-02-08 00:05:46 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,29 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // <--- AJOUTE CETTE LIGNE ICI
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  reactStrictMode: false,
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        pathname: '/**',
-      },
-    ],
-  },
-  experimental: {
-    webpackBuildWorker: true,
   },
 };
 
