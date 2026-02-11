f18ff666a0193eb4866379e636fe80c6a6029b62
stayflow2025-arch <stayflow2025@gmail.com>
2026-02-11 13:56:42 +0100

    Update opennext.config.ts

--- opennext.config.ts
+++ opennext.config.ts
@@ -1,3 +1,12 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import type { OpenNextConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({});
+const config: OpenNextConfig = {
+  output: "standalone",
+  cloudflare: {
+    worker: {
+      name: "stayfloow",
+    },
+  },
+};
+
+export default config;
