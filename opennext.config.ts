8ccaa5b56e570aaf57531ce00719dfa74b8fae67
stayfloiw <stayflow2025@gmail.com>
2026-02-14 12:15:19 +0100

    fix opennext config

--- opennext.config.ts
+++ opennext.config.ts
@@ -1,7 +1,5 @@
 import type { OpenNextConfig } from "@opennextjs/cloudflare";
 
-const config: OpenNextConfig = {
-  output: "standalone",
-};
+const config: OpenNextConfig = {};
 
 export default config;
